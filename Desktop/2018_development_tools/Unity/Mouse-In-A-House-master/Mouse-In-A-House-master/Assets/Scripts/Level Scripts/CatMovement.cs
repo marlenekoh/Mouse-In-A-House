065d7ac6db634a65da30a7f01bd1aeaccdb32be6
zufarsk8 <a0130359@u.nus.edu>
2018-10-18 18:11:36 +0800

    add assets and animation

---
+++ Desktop/2018_development_tools/Unity/Mouse-In-A-House-master/Mouse-In-A-House-master/Assets/Scripts/Level Scripts/CatMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CatMovement : MonoBehaviour
+{
+    //public LayerMask enemyMask;
+    public float leftLimit, rightLimit;
+
+    protected Rigidbody2D rb;
+    protected Transform trans;
+    protected Animator anim;
+    protected GameObject mouse;
+    protected int speed;
+    protected int maxSpeed; //to vary according to adaptive difficulty
+
+    protected void Start()
+    {
+        mouse = GameObject.Find("Mouse");
+        trans = this.transform;
+        rb = this.GetComponent<Rigidbody2D>();
+        Random.InitState(System.DateTime.Now.Millisecond);
+        speed = Random.Range(1, GameManager.getInstance().getMaxSpeed());
+        //myAnim = this.GetComponent<Animator>();
+    }
+
+    protected void FixedUpdate()
+    {
+        //Always move forward
+        Vector2 vel = rb.velocity;
+        vel.x = trans.right.x * speed;
+        if (trans.localScale.x < 0)
+        {
+            vel.x *= -1;
+        }
+        rb.velocity = vel;
+
+        // to destroy cat once it goes out of screen
+        if (trans.position.x < leftLimit || trans.position.x > rightLimit) // kill cat if cat goes out of screen
+        {
+            Destroy(gameObject);
+            GameManager.getInstance().spawnCat();
+        }
+    }
+}
---
+++ Desktop/2018_development_tools/Unity/Mouse-In-A-House-master/Mouse-In-A-House-master/Assets/Scripts/Level Scripts/PlayerManager.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    public float speedX;
+    public float jumpSpeedY;
+
+    bool facingRight = true;
+    bool goingRight = true;
+    bool isJumping = false;
+
+    Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * speedX;
+
+        transform.Translate(x, 0, 0);
+
+
+        // move right, stop
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            goingRight = false;
+        }
+
+        // move left, stop
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            goingRight = true;
+        }
+
+        if (!isJumping && Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = true;
+            rb.AddForce(new Vector2(rb.velocity.x, jumpSpeedY));
+        }
+
+        // add fake gravity to land faster
+        if (isJumping)
+        {
+            Vector3 vel = rb.velocity;
+            vel.y -= 15 * Time.deltaTime;
+            rb.velocity = vel;
+        }
+
+        // Flip
+        if (goingRight && !facingRight || !goingRight && facingRight)
+        {
+            facingRight = !facingRight;
+            Vector3 temp = transform.localScale;
+            temp.x *= -1;
+            transform.localScale = temp;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // for jump
+        if (collision.gameObject.tag == "Ground" && rb.velocity.y == 0)
+        {
+            rb.velocity = new Vector3(0,0,0);
+            isJumping = false;
+        }
+
+        // if mouse touches bottom of cat
+        if (collision.gameObject.tag == "Cat") // then kill player
+        {
+            // game over code
+            Debug.Log("Game over!");
+            GameManager.getInstance().gameOver();
+        }
+
+    }
+    
+    // on collision with other cats
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "CatBack") // then kill cat
+        {
+            Debug.Log("You killed a cat!");
+            // TODO: increment cat kill count
+            // TODO: implement stun all cats when one cat dies
+            Destroy(collision.gameObject.GetComponent<Transform>().parent.gameObject);
+            GameManager.getInstance().spawnCat();
+        }
+
+        // if mouse touches front of cat
+        if (collision.gameObject.tag == "Cat") // then game over
+        {
+            // game over code
+            Debug.Log("Game over!");
+            GameManager.getInstance().gameOver();
+        }
+    }
+}
